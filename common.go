91ebc1e407c6ff1d4cb73f59ead0b622ee53e48f
NNagato <darkdragon236@gmail.com>
2022-08-02 15:49:22 +0700

    update market response

--- common.go
+++ common.go
@@ -87,26 +87,31 @@ type Position struct {
 
 type Market struct {
 	Name                  string     `json:"name"`
+	Enabled               bool       `json:"enabled"`
+	PostOnly              bool       `json:"postOnly"`
+	PriceIncrement        float64    `json:"priceIncrement"`
+	SizeIncrement         float64    `json:"sizeIncrement"`
+	MinProvideSize        float64    `json:"minProvideSize"`
+	Last                  float64    `json:"last"`
+	Bid                   float64    `json:"bid"`
+	Ask                   float64    `json:"ask"`
+	Price                 float64    `json:"price"`
+	Type                  MarketType `json:"type"`
+	FutureType            *string    `json:"futureType"`
 	BaseCurrency          *string    `json:"baseCurrency"`
+	IsEtfMarket           bool       `json:"isEtfMarket"`
 	QuoteCurrency         *string    `json:"quoteCurrency"`
-	QuoteVolume24H        float64    `json:"quoteVolume24h"`
+	Underlying            *string    `json:"underlying"`
+	Restricted            bool       `json:"restricted"`
+	HighLeverageFeeExempt bool       `json:"highLeverageFeeExempt"`
+	LargeOrderThreshold   float64    `json:"largeOrderThreshold"`
 	Change1H              float64    `json:"change1h"`
 	Change24H             float64    `json:"change24h"`
 	ChangeBod             float64    `json:"changeBod"`
-	HighLeverageFeeExempt bool       `json:"highLeverageFeeExempt"`
-	MinProvideSize        float64    `json:"minProvideSize"`
-	Type                  MarketType `json:"type"`
-	Underlying            string     `json:"underlying"`
-	Enabled               bool       `json:"enabled"`
-	Ask                   float64    `json:"ask"`
-	Bid                   float64    `json:"bid"`
-	Last                  float64    `json:"last"`
-	PostOnly              bool       `json:"postOnly"`
-	Price                 float64    `json:"price"`
-	PriceIncrement        float64    `json:"priceIncrement"`
-	SizeIncrement         float64    `json:"sizeIncrement"`
-	Restricted            bool       `json:"restricted"`
+	QuoteVolume24H        float64    `json:"quoteVolume24h"`
 	VolumeUsd24H          float64    `json:"volumeUsd24h"`
+	PriceHigh24H          float64    `json:"priceHigh24h"`
+	PriceLow24H           float64    `json:"priceLow24h"`
 }
 
 type Balance struct {
